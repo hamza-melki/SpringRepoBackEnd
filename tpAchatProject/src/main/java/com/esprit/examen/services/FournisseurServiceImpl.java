9b01471e14101bdc2298981008d153e5d00e796b
RahmaBg <86840986+RahmaBg@users.noreply.github.com>
2023-04-10 22:12:34 +0100

    Update FournisseurServiceImpl.java

--- tpAchatProject/src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
+++ tpAchatProject/src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
@@ -78,6 +78,7 @@ public class FournisseurServiceImpl implements IFournisseurService {
 	public void assignSecteurActiviteToFournisseur(Long idSecteurActivite, Long idFournisseur) {
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
 		SecteurActivite secteurActivite = secteurActiviteRepository.findById(idSecteurActivite).orElse(null);
+		if( fournisseur != null)
         fournisseur.getSecteurActivites().add(secteurActivite);
         fournisseurRepository.save(fournisseur);
 		
